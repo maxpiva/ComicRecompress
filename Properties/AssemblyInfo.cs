584d1210f6ae1c54301abde64c9dbce64700c815
Maximo Piva <mpiva@adventintermodal.com>
2024-08-27 16:14:42 -0300

    Remove Copyright generated by CommandLine

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ComicRecompress")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
